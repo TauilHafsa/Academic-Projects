59a8d345c8e5510fb2c5e1e5879bf13b43a25019
TauilHafsa <150071317+TauilHafsa@users.noreply.github.com>
2024-04-22 15:07:26 +0100

    Add files via upload

---
+++ Automaton manipulation/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "prototype.h"
+
+/*
+    taille: -taille[0] : nombre de transitions
+            -taille[1] : nombre des états initiaux
+            -taille[2] : nombre des états finaux
+            -taille[3] : nombre des états
+*/
+
+/*----------------------MAIN -----------------------------*/
+
+int main()
+{
+
+    menuPrincipal();
+
+    return 0;
+}
+
+
+
